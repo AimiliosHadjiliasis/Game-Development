39a04bf307cd402ccc250f30466791ac28e4227c
AimiliosHadjiliasis <47337615+AimiliosHadjiliasis@users.noreply.github.com>
2020-01-09 21:41:14 +0200

    Update Exercise5Part2.cpp

--- Path Finding Algorithms/Exercise5Part2.cpp
+++ Path Finding Algorithms/Exercise5Part2.cpp
@@ -1,14 +1,11 @@
-#include "stdafx.h"
-#include <deque>
 #include <iostream>
-#include <list>
+#include <deque>
+
 using namespace std;
 using std::deque;
 
 
-//(1) Create a coords Structure
-struct coords
-{
+struct coords {
 	int x;
 	int y;
 };
@@ -25,22 +22,6 @@ void display(deque <coords*> openList)
 	}
 }
 
-bool check(deque <coords*> openList, int num1, int num2)
-{
-	deque <coords*> ::iterator p;
-	p = openList.begin();
-	while (p != openList.end())
-	{
-		if (((*p)->x == num1) && ((*p)->y == num2))
-		{
-			return 1;
-		}
-		p++;
-	}
-
-	return 0;
-}
-
 void freeMemory(deque <coords*> openList, coords* tmp)
 {
 	deque <coords*> ::iterator p;
@@ -53,16 +34,29 @@ void freeMemory(deque <coords*> openList, coords* tmp)
 	}
 }
 
-int main()
-{	
-	//(2) Declare a deque of type coords pointer and call it 'openList'
-	deque <coords*> openList;
+bool findFunction(deque <coords*> openList, coords* tmp)
+{
+	deque <coords*> ::iterator p;
+	p = openList.begin();
 
-	/*
-	(3)	Assign 10 new items to 'openList'. I would suggest that you use a for loop and assign
-	the values ( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) to the x fields of the list and ( 0, 2, 4, 6, 8, 10,
-	12,	14, 16, 18 ) to the y fields. Remember to allocate memory to each element of the list.
-	*/
+	int i = 0;
+	while (p != openList.end())
+	{
+		if (tmp->x == openList[i]->x && tmp->y == openList[i]->y)
+		{
+			return true;
+		}
+		p++;
+		i++;
+	}
+	return false;
+}
+
+
+int main()
+{
+	deque<coords*> openList;
+	
 	coords* tmp;
 	for (int i = 0; i <= 9; i++)
 	{
@@ -72,54 +66,29 @@ int main()
 		openList.push_back(tmp);
 	}
 
-	//(4)  Declare a second list using deque of type coords pointer and call it 'closedList'
-	deque <coords*> closeList;
 
-
-	/*
-	(5)  Transfer over the data from the first list to the second list. In other words, assign the
-	pointers from the openList to the closedList. You will need an iterator to go through the
-	openList. You do not need to allocate memory during this operation - you are merely
-	making the pointers in the second list point at the same data as the pointers in the
-	original list.
-	*/
-	deque <coords*> ::iterator p;
-	p = openList.begin();
-
-	while (p != openList.end())
-	{	
-		
-		closeList.push_back((*p));
-		p++;
-	}
-
-	//(6)	Output the value for x and y for all of the elements of the closedList.
-	cout << "OpenList:" << endl;
-	cout << "------------------" << endl;
 	display(openList);
+	int x, y;
 
-	cout << "\nCloseList:" << endl;
-	cout << "------------------" << endl;
-	display(closeList);
+	coords* tmpFind = new(coords);
+	cout << '\n';
+	cout << "Check whether a node exist:" << endl;
+	cout << "XCord: ";
+	cin >> tmpFind->x;
+	cout << "YCord: ";
+	cin >> tmpFind->y;
 	
-	//(5) 	Construct find function.
-	cout << "Give x:" << endl;
-	cin >> num1;
-	"/n";
-	cout << "Give y:" << endl;
-	cin >> num2;
-	"/n";
 
-	if (check(openList, num1, num2))
+	if (findFunction(openList, tmpFind))
 	{
-		cout << "Exist" << endl;
+		cout << "Node Found!" << endl;
 	}
 	else
 	{
-		cout << "Not Exist" << endl;
+		cout << "Node Not Found!" << endl;
 	}
 
-	freeMemory(closeList, tmp);
-	cout << closeList.front()->x << " " << closeList.front()->y << endl;
-    return 0;
+	freeMemory(openList, tmp);
+
+	return 0;
 }
